afe4d36cb2fdb3f19b44a2178ccdf85d1a6a9763
Cho Raku <ai.fake.news.portal@gmail.com>
2025-04-21 00:47:55 +0900

    revised for ts

--- ref_20250421/QrCodeScan.vue
+++ ref_20250421/QrCodeScan.vue
@@ -2,39 +2,39 @@
     <div>
       <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
       <p v-if="decodedContent">Scanned Content: {{ decodedContent }}</p>
+      <button v-if="error" @click="retryInitialization">Retry</button>
     </div>
   </template>
   
-  <script>
+  <script setup lang="ts">
+  import { ref } from 'vue';
   import { QrcodeStream } from 'vue-qrcode-reader'
   
-  export default {
-    components: {
-      QrcodeStream
-    },
-    data() {
-      return {
-        decodedContent: null
+  const decodedContent = ref(null);
+  const error = ref(null);
+
+  function onDecode(content) {
+    decodedContent.value = content;
+  }
+
+  function onInit(promise) {
+    promise.catch(err => {
+      if (err.name === 'NotAllowedError') {
+        alert('Please allow camera access to scan QR codes.');
+      } else {
+        alert('An error occurred while initializing the QR scanner.');
       }
-    },
-    methods: {
-      onDecode(content) {
-        this.decodedContent = content
-      },
-      onInit(promise) {
-        promise.catch(error => {
-          if (error.name === 'NotAllowedError') {
-            alert('Please allow camera access to scan QR codes.')
-          } else {
-            alert('An error occurred while initializing the QR scanner.')
-          }
-        })
-      }
-    }
+      error.value = err;
+    });
+  }
+
+  function retryInitialization() {
+    error.value = null;
+    // Assume there is a method to restart the QR code stream
+    restartQrCodeStream();
   }
   </script>
   
   <style scoped>
   /* Add any styles you need here */
   </style>
-  
--- ref_20250421/QrCodeScanPage.vue
+++ ref_20250421/QrCodeScanPage.vue
@@ -1,28 +1,25 @@
 <template>
-    <div>
-      <button @click="showScanner = !showScanner">
-        {{ showScanner ? 'Hide' : 'Show' }} QR Scanner
-      </button>
-      <QrScanner v-if="showScanner" />
-    </div>
-  </template>
-  
-  <script>
-  import QrScanner from '~/components/QrScanner.vue'
-  
-  export default {
-    components: {
-      QrScanner
-    },
-    data() {
-      return {
-        showScanner: false
-      }
-    }
-  }
-  </script>
-  
-  <style scoped>
-  /* Add any styles you need here */
-  </style>
-  
+  <div>
+    <button @click="toggleScanner">
+      {{ buttonText }} QR Scanner
+    </button>
+    <QrScanner v-if="showScanner" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import QrScanner from './QrCodeScanner.vue'; 
+
+const showScanner = ref(false);
+
+function toggleScanner() {
+  showScanner.value = !showScanner.value;
+}
+
+const buttonText = computed(() => showScanner.value ? 'Hide' : 'Show');
+</script>
+
+<style scoped>
+/* Add any styles you need here */
+</style>
